798f498e8ac349d848c3586500f7f0ecd83a4f5b
Félix Chiasson <github@felixchiasson.ca>
2025-02-05 09:17:33 -0500

    Revert "Move some utility around"
    This reverts commit 4119e8d4f46fb137f5c1ca7938f395c102f17a55.

--- src/api/cache.rs
+++ src/api/cache.rs
@@ -1,6 +1,5 @@
 use crate::api::omdb::OMDBResponse;
 use crate::api::tmdb::TVShowDetails;
-use crate::utils::serde::timestamp;
 use dashmap::DashMap;
 use serde::{Deserialize, Serialize};
 use std::fs;
@@ -15,8 +14,29 @@ const CACHE_TTL: Duration = Duration::from_secs(24 * 60 * 60);
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct CachedItem<T> {
     pub data: T,
-    #[serde(with = "timestamp")]
+    #[serde(with = "timestamp_serde")]
     pub timestamp: Instant,
+}
+
+mod timestamp_serde {
+    use serde::{Deserialize, Deserializer, Serializer};
+    use std::time::{Duration, Instant};
+
+    pub fn serialize<S>(instant: &Instant, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let duration = instant.elapsed();
+        serializer.serialize_u64(duration.as_secs())
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Instant, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let secs = u64::deserialize(deserializer)?;
+        Ok(Instant::now() - Duration::from_secs(secs))
+    }
 }
 
 #[derive(Serialize, Deserialize)]
